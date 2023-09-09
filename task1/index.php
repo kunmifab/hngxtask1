87c19cd7230aa574db69e09c7b4a2782959b3a2d
Akinola Farouq Akinkunmi <akin.tenplus@gmail.com>
2023-09-09 17:59:35 +0100

    first commit

---
+++ task1/index.php
@@ -0,0 +1,30 @@
+<?php
+if (isset($_GET['slack_name']) && isset($_GET['track'])) {
+    $slack_name = $_GET['slack_name'];
+    $track = $_GET['track'];
+    $currentTimeUTC = gmdate('Y-m-d\TH:i:s\Z', time());
+    $current_day = date('l');
+    $response = array(
+        "slack_name"=> $slack_name,
+        "current_day"=> $current_day,
+        "utc_time"=> $currentTimeUTC,
+        "track"=> $track,
+        "github_file_url"=> "https://github.com/username/repo/blob/main/file_name.ext",
+        "github_repo_url"=> "https://github.com/username/repo",
+        "status_code"=> 200
+    );
+    {
+        
+      }
+
+    header('Content-Type: application/json');
+
+    echo json_encode($response, JSON_UNESCAPED_SLASHES);
+} else {
+    $error_response = array(
+        'error' => 'Both name and age parameters are required.'
+    );
+    header('Content-Type: application/json');
+    echo json_encode($error_response);
+}
+?>
